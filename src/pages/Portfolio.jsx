d2525bef700abfcdd1f1e3b8c24cb1edf3a899fa
pati-lima <pati.lima0122@gmail.com>
2022-11-29 11:03:11 -0300

    Adiciona flexbox pagina de portifólio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,25 +9,25 @@ export function Portfolio () {
       <Header title="Meus Projetinhos" image={portfolioImg} />
       <div className={styles.cardsContainer} >
         <div className={styles.itensContainer}>
-        <h2>Página de Filmes <img 
-        className={styles.imgContainer}src="https://media.giphy.com/media/quw6LT1JM4FQWbzDfh/giphy.gif" alt="filmes" target='black'/></h2>
+        <h2> Filmes <img 
+        className={styles.imgContainer}src="https://media.giphy.com/media/g9vQ5N2fmuB245NRWk/giphy.gif" alt="filmes" target='black'/></h2>
         <p>
           Página criada durante o curso de Front-end da Reprograma 
-          no modulo de Javascript 
+          
           </p>
         <a className={styles.links} href="https://ghibliflix.netlify.app/" target='blank'>Clique aqui!</a>
         </div>
 
         <div className={styles.itensContainer}>
-        <h2>To-Do List <img className={styles.imgContainer} src="https://media.giphy.com/media/XK0nSxvMJfPChUMWIg/giphy.gif" alt="todolist" target='black' /></h2>
+        <h2>To-Do List <img className={styles.imgContainer} src="https://media.giphy.com/media/Ed848hrDHv5MnVRIwE/giphy.gif" alt="todolist" target='black' /></h2>
         <p> Uma listinha de tarefas bem charmosa utilizando Javascript</p>
         <a className={styles.links} href="https://to-do-listpaty.netlify.app/" >Clique aqui!</a>
         </div>
 
         <div className={styles.itensContainer}>
-        <h2>Meus Links <img className={styles.imgContainer} src="https://media.giphy.com/media/qux62wosdSZaHxV0ug/giphy.gif" alt="meuslinks"target='blank' /></h2>
+        <h2>Meus Links <img className={styles.imgContainer} src="https://media.giphy.com/media/2CA2k57j4YkWLNwOnW/giphy.gif" alt="meuslinks"target='blank' /></h2>
         <p>
-           Por aqui é possivel entrar em contato comigo pelas minhas redes sociais.
+           Entre em contato comigo pelas minhas redes sociais.
         </p>
         <a className={styles.links} href="https://mylinkspaty.netlify.app/" >Clique aqui!</a>
         
