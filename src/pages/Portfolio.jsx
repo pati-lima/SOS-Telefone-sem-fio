60043ebb7feeb773f1bf4fa93d4ccabfc25a0f23
pati-lima <pati.lima0122@gmail.com>
2022-11-29 19:12:57 -0300

    Modifica atributos na página portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,26 +10,25 @@ export function Portfolio () {
       <div className={styles.cardsContainer} >
         <div className={styles.itensContainer}>
         <h2> Filmes <img 
-        className={styles.imgContainer}src="https://media.giphy.com/media/g9vQ5N2fmuB245NRWk/giphy.gif" alt="filmes" target='black'/></h2>
+        className={styles.imgContainer}src="https://media.giphy.com/media/g9vQ5N2fmuB245NRWk/giphy.gif" alt="filmes" /></h2>
         <p>
           Página criada durante o curso de Front-end da Reprograma 
-          
           </p>
         <a className={styles.links} href="https://ghibliflix.netlify.app/" target='blank'>Clique aqui!</a>
         </div>
 
         <div className={styles.itensContainer}>
-        <h2>To-Do List <img className={styles.imgContainer} src="https://media.giphy.com/media/Ed848hrDHv5MnVRIwE/giphy.gif" alt="todolist" target='black' /></h2>
+        <h2>To-Do List <img className={styles.imgContainer} src="https://media.giphy.com/media/Ed848hrDHv5MnVRIwE/giphy.gif" alt="todolist" target='blank' /></h2>
         <p> Uma listinha de tarefas bem charmosa utilizando Javascript</p>
-        <a className={styles.links} href="https://to-do-listpaty.netlify.app/" >Clique aqui!</a>
+        <a className={styles.links} href="https://to-do-listpaty.netlify.app/" target='blank' >Clique aqui!</a>
         </div>
 
         <div className={styles.itensContainer}>
-        <h2>Meus Links <img className={styles.imgContainer} src="https://media.giphy.com/media/2CA2k57j4YkWLNwOnW/giphy.gif" alt="meuslinks"target='blank' /></h2>
+        <h2>Meus Links <img className={styles.imgContainer} src="https://media.giphy.com/media/2CA2k57j4YkWLNwOnW/giphy.gif" alt="meuslinks" target='blank' /></h2>
         <p>
            Entre em contato comigo pelas minhas redes sociais.
         </p>
-        <a className={styles.links} href="https://mylinkspaty.netlify.app/" >Clique aqui!</a>
+        <a className={styles.links} href="https://mylinkspaty.netlify.app/" target='blank' >Clique aqui!</a>
         
         </div>
        
