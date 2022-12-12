b3ca34b6f62d69dc81c84ee24da65eebec2b3d48
pati-lima <pati.lima0122@gmail.com>
2022-12-12 19:45:44 -0300

    Insere style components sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -10,11 +10,26 @@ import livroEster from '../assets/livroester.png'
 export function Sobre() {
   return (
     <>
-      <Header title ="Sobre Mim" image ={blackGirlImg} />
+      <Header title="Sobre Mim" image={blackGirlImg} />
       <div className={styles.sobreContainer}>
-        <img className={styles.bioImage} src={esterImg}/>
-      <div className={styles.bioContainer}>
-      <h2 className={styles.bioTitle}>
+        <img className={styles.bioImage} src={esterImg} />
+        <div className={styles.bioContainer}>
+          <h2 className={styles.bioTitle}>
+            Ester Ribeiro , prazer!
+          </h2>
+          <p className={styles.bioText}>Olá, é um prazer te ter por aqui.
+            Já que compartilharemos algumas confidenciais, deixe me começar. Sou nova no mundo dos escritores, apaixonada por pets, séries orientais e vinho! A arte, a música e histórias me deixam intrigada e sou uma amante fiel do café. Prefiro
+            inverno e os dias de chuva são os meus dias mais produtivos!
+          </p>
+
+
+        </div>
+
+      </div>
+      {/* <div className={styles.sobreContainer}>
+        <img className={styles.bioImage} src={escritoraImg} />
+        <div className={styles.bioContainer}>
+          <h2 className={styles.bioTitle}>
             Ester Ribeiro , prazer!
           </h2>
           <p className={styles.bioText}>
@@ -42,65 +57,18 @@ export function Sobre() {
 
         </div>
 
-      </div>
+      </div> */}
       <div className={styles.sobreContainer}>
-        <img className={styles.bioImage} src={escritoraImg}/>
-      <div className={styles.bioContainer}>
-      <h2 className={styles.bioTitle}>
-            Ester Ribeiro , prazer!
+        <img className={styles.bioImage} src={livroEster} />
+        <div className={styles.bioContainer}>
+          <h2 className={styles.bioTitle}>
+          Ah, conhece meu livro? 
+
           </h2>
           <p className={styles.bioText}>
-
-
-            <li className={styles.sobreList}>
-              Quase psi.
-            </li>
-            <li className={styles.sobreList}>
-              Escrevi um livro.
-            </li>
-            <li className={styles.sobreList}>
-              Apaixonada por pets, séries e um bom vinho.
-            </li >
-            <li className={styles.sobreList}>
-              Super curiosa em conhecer histórias.
-
-            </li>
-            <li className={styles.sobreList}>
-              Não recuso um café
-            </li>
-
-          </p>
-
-
-        </div>
-
-      </div>
-      <div className={styles.sobreContainer}>
-        <img className={styles.bioImage} src={livroEster}/>
-      <div className={styles.bioContainer}>
-      <h2 className={styles.bioTitle}>
-            Sobre o livro
-          </h2>
-          <p className={styles.bioText}>
-          Caminhos de vidro aborda sobre a linha tênue da vida e da morte. Um livro composto por duas partes 
-          surpreendentes e cheios de relatos mais que emocionantes
-{/* 
-            <li className={styles.sobreList}>
-              Quase psi.
-            </li>
-            <li className={styles.sobreList}>
-              Escrevi um livro.
-            </li>
-            <li className={styles.sobreList}>
-              Apaixonada por pets, séries e um bom vinho.
-            </li >
-            <li className={styles.sobreList}>
-              Super curiosa em conhecer histórias.
-
-            </li>
-            <li className={styles.sobreList}>
-              Não recuso um café
-            </li> */}
+          Caminhos de vidro te mostra que o luto é bem mais presente na sua vida do que imagina. Trazendo duas faces, uma para pessoas em crise e outra para aqueles que querem 
+          entender mais sobre os lutos. Sim, lutos! Existe mais de um e eu te explico quais são.
+            
 
           </p>
 
