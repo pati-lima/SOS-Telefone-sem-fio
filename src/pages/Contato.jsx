76a2422f1da4eca7fda048dcf913ff4f4707fbce
pati-lima <pati.lima0122@gmail.com>
2023-01-09 11:06:09 -0300

    Atualizando pagina de contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -3,10 +3,11 @@ import { useState, UseState } from 'react'
 
 import { Header} from '../components/Header'
 import contatoImg from '../assets/contato.png'
-import {database} from '../service/firebase'
-import { ref, push, set} from 'firebase/database'
+// import {database} from '../service/firebase'
+// import { ref, push, set} from 'firebase/database'
 import { SiInstagram  } from 'react-icons/si'
 import { SiLinkedin }  from 'react-icons/si'
+import { FaWhatsappSquare }  from 'react-icons/fa'
 
 
 import styles from '../styles/pages/contato.module.css'
@@ -16,6 +17,8 @@ import  {  ToastContainer ,  toast  }  from  'react-toastify' ;
 const notify = () =>{
   toast("Recebi sua mensagem, em breve entrarei em contato!")
 }
+
+
 
 export function Contato() {
   const [nome, setNome] = useState('')
@@ -55,29 +58,30 @@ export function Contato() {
       <Header title image={contatoImg}className={styles.imgcontato} />
       <div className={styles.containerForm}>
         <form  className={styles.form} onSubmit={handleCreateMessage}>
-        <h3  className={styles.textform}>Chama no Contatinho!</h3>
-          <input 
+        <h3  className={styles.textform}>Fale comigo através das minhas redes sociais! </h3>
+          {/* <input 
             onChange={handleInputValueNome} 
             placeholder="Como gostaria de ser chamado (a)?"
             value={nome}
             className={styles.formInput}
-          />
-          <input 
+          /> */}
+          {/* <input 
             onChange={handleInputValueTelefone} 
             placeholder="Digite um telefone pra contato!"
             value={telefone}
             className={styles.formInput}
-          />
-          <textarea 
+          /> */}
+          {/* <textarea 
             onChange={handleInputValueMensagem} 
             placeholder="Gostaria de Deixar alguma observação?"
             value={mensagem}
             className={styles.formTextArea}
-          />
-          <button onClick={notify} className={styles.formButton} type="submit">Enviar mensagem</button>
-            <ToastContainer /> 
-              <p className={styles.paragrafoContato}>Conecte-te comigo nas minhas redes! </p>
+          /> */}
+          {/* <button onClick={notify} className={styles.formButton} type="submit">Enviar mensagem </button>
+            <ToastContainer />  */}
+              {/* <p className={styles.paragrafoContato}>Fale comigo através das minhas redes! </p> */}
         <div className ={styles.redessociais}>
+        <a className={styles.iconesRedesSociais} href ="https://contate.me/sos-telefone-sem-fio">< FaWhatsappSquare></FaWhatsappSquare></a>
         <a  className={styles.iconesRedesSociais}  href   =  "https://www.instagram.com/psi_estercosta/ " ><SiInstagram></SiInstagram></a>
         <a className={styles.iconesRedesSociais} href ="https://www.linkedin.com/in/ester-ribeiro-da-costa-a5a094200/">< SiLinkedin></SiLinkedin></a>
         </div>
